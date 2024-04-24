a702b07508396f9eaa943f03501ea5f0da1f2134
frank <2089158462@qq.com>
2024-04-24 14:38:37 +0800

    通用返回对象 自定义错误码

---
+++ src/main/java/com/jxh/usercenter/common/ResultUtils.java
@@ -0,0 +1,29 @@
+package com.jxh.usercenter.common;
+
+/**
+ * 返回工具类
+ *
+ * @author 20891
+ */
+public class ResultUtils {
+
+    /**
+     * 成功
+     * @param data
+     * @return
+     * @param <T>
+     */
+    public static <T> BaseResponse<T> success(T data) {
+        return new BaseResponse<>(0,data,"ok");
+    }
+
+    /**
+     * 失败
+     * @param errorCode
+     * @return
+     */
+    public static BaseResponse error(ErrorCode errorCode) {
+        return new BaseResponse<>(errorCode);
+    }
+
+}
--- src/main/java/com/jxh/usercenter/controller/UserController.java
+++ src/main/java/com/jxh/usercenter/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.jxh.usercenter.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.jxh.usercenter.common.BaseResponse;
+import com.jxh.usercenter.common.ResultUtils;
 import com.jxh.usercenter.constant.UserConstant;
 import com.jxh.usercenter.model.domain.User;
 import com.jxh.usercenter.model.request.UserLoginRequest;
@@ -11,7 +13,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,7 +29,7 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/register")
-    public Long userRegister(@RequestBody UserRegisterRequest userRegisterRequest){
+    public BaseResponse<Long> userRegister(@RequestBody UserRegisterRequest userRegisterRequest) {
 
         if (userRegisterRequest == null) {
             return null;
@@ -37,14 +38,16 @@ public class UserController {
         String userAccount = userRegisterRequest.getUserAccount();
         String userPassword = userRegisterRequest.getUserPassword();
         String checkPassword = userRegisterRequest.getCheckPassword();
-        if (StringUtils.isAnyBlank(userAccount,userPassword,checkPassword)){
+        if (StringUtils.isAnyBlank(userAccount, userPassword, checkPassword)) {
             return null;
         }
-        return userService.userRegister(userAccount,userPassword,checkPassword);
+        long result = userService.userRegister(userAccount, userPassword, checkPassword);
+
+        return ResultUtils.success(result);
     }
 
     @PostMapping("/login")
-    public User userLogin(@RequestBody UserLoginRequest userLoginRequest, HttpServletRequest request){
+    public BaseResponse<User> userLogin(@RequestBody UserLoginRequest userLoginRequest, HttpServletRequest request) {
 
         if (userLoginRequest == null) {
             return null;
@@ -52,24 +55,27 @@ public class UserController {
 
         String userAccount = userLoginRequest.getUserAccount();
         String userPassword = userLoginRequest.getUserPassword();
-        if (StringUtils.isAnyBlank(userAccount,userPassword)){
+        if (StringUtils.isAnyBlank(userAccount, userPassword)) {
             return null;
         }
-        return userService.userLogin(userAccount,userPassword,request);
+        User user = userService.userLogin(userAccount, userPassword, request);
+
+        return ResultUtils.success(user);
     }
 
     @PostMapping("/logout")
-    public Integer userLogout(HttpServletRequest request){
+    public BaseResponse<Integer> userLogout(HttpServletRequest request) {
 
         if (request == null) {
             return null;
         }
+        int result = userService.userLogout(request);
 
-        return userService.userLogout(request);
+        return ResultUtils.success(result);
     }
 
     @GetMapping("/current")
-    public User currentUser(HttpServletRequest request) {
+    public BaseResponse<User> currentUser(HttpServletRequest request) {
         User user = (User) request.getSession().getAttribute(UserConstant.USER_LOGIN_STATE);
         if (user == null) {
             return null;
@@ -77,31 +83,36 @@ public class UserController {
         //TODO 校验用户是否合法
         long userId = user.getId();
         User newUser = userService.getById(userId);
-        return userService.getSafetyUser(newUser);
+        User safetyUser = userService.getSafetyUser(newUser);
+
+        return ResultUtils.success(safetyUser);
     }
 
     @GetMapping("/search")
-    public List<User> searchUsers(String username,HttpServletRequest servletRequest) {
-        if (!isAdmin(servletRequest)){
-            return new ArrayList<>();
+    public BaseResponse<List<User>> searchUsers(String username, HttpServletRequest servletRequest) {
+        if (!isAdmin(servletRequest)) {
+            return null;
         }
         QueryWrapper<User> userQueryWrapper = new QueryWrapper<>();
-        if (StringUtils.isNotBlank(username)){
-            userQueryWrapper.like("username",username);
+        if (StringUtils.isNotBlank(username)) {
+            userQueryWrapper.like("username", username);
         }
         List<User> userList = userService.list(userQueryWrapper);
-        return userList.stream().map(user -> userService.getSafetyUser(user)).collect(Collectors.toList());
+        List<User> result = userList.stream().map(user -> userService.getSafetyUser(user)).collect(Collectors.toList());
+        return ResultUtils.success(result);
     }
 
     @PostMapping("/delete")
-    public boolean deleteUser(@RequestBody long id,HttpServletRequest servletRequest) {
+    public BaseResponse<Boolean> deleteUser(@RequestBody long id, HttpServletRequest servletRequest) {
         if (!isAdmin(servletRequest)) {
-            return false;
+            return null;
         }
-        if (id <= 0){
-            return false;
+        if (id <= 0) {
+            return null;
         }
-        return userService.removeById(id);
+        boolean result = userService.removeById(id);
+
+        return ResultUtils.success(result);
     }
 
     /**
